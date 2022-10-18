e9895a7eb8d4c475eaff061be1a2d1d5c7f063e0
Michael Crandall <mec192@gmail.com>
2022-10-17 23:03:48 -0500

    added prompt and questions

--- Develop/index.js
+++ Develop/index.js
@@ -27,3 +27,56 @@ const gernerateReadMe = ({ title, description, instructions, usage, contribution
     ## Git hub username: ${questions}
     
     ## Email for more info: ${email}`;
+
+    inquirer
+    .prompt ([
+        { 
+            name: 'title',
+            message: 'What is the title of your project?',
+            type: 'input',
+
+
+        },
+
+        {
+            name: 'description',
+            message: 'Write a short description of what your project does',
+            type: 'input',
+        },
+
+        {
+            name: 'instructions',
+            message: 'Please enter any instructions needed for project to function',
+            type: 'input'
+        },
+
+        {
+            name: 'usage',
+            message: 'Enter any usage information here',
+            type: 'input'
+        },
+
+        {
+            name: 'contribution',
+            message: 'Enter the constribution guidlines of this project',
+            type: 'input'
+        },
+        {
+            name: 'test',
+            message: 'Enter test instructions needed for this project',
+            type: 'input'
+        },
+        {
+            name: 'license',
+            message: 'which license are you using',
+            type: 'input'
+        },
+        {
+            name: 'questions',
+            message: 'enter your github username',
+            type:'input'
+        },
+        {    name: 'email',
+             message: 'enter email',
+             type: 'input'
+    },
